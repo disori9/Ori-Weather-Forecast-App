6e837bbecbfbf728c124e222f34daa6b4a3a5414
Ori <disori777@gmail.com>
2024-11-13 09:53:40 +0800

    Add text_input, modify placeholder in select_data

--- main.py
+++ main.py
@@ -2,7 +2,8 @@ import streamlit as st
 
 
 st.title("Weather Forecast for the Next Days")
+st.text_input("Place:", placeholder="Input a place to view data")
 forecast_days = st.slider("Forecast Days", 1, 5, 1)
 data_choice = ["Temperature", "Weather"]
-select_data = st.selectbox("Select data to view", data_choice, index=None, placeholder="Select data")
+select_data = st.selectbox("Select data to view", data_choice, index=None, placeholder="What data you want to see?")
 st.title("Temperature for the next 2 days in ")
