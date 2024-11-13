358dd449e65968792d0816688cb5625c05ebc94d
Ori <disori777@gmail.com>
2024-11-13 09:56:00 +0800

    Add dynamically changing variables in st.tile, selecting data, day, and place

--- main.py
+++ main.py
@@ -2,8 +2,8 @@ import streamlit as st
 
 
 st.title("Weather Forecast for the Next Days")
-st.text_input("Place:", placeholder="Input a place to view data")
+place = st.text_input("Place:", placeholder="Input a place to view data")
 forecast_days = st.slider("Forecast Days", 1, 5, 1)
 data_choice = ["Temperature", "Weather"]
-select_data = st.selectbox("Select data to view", data_choice, index=None, placeholder="What data you want to see?")
-st.title("Temperature for the next 2 days in ")
+select_data = st.selectbox("Select data to view", data_choice, placeholder="What data you want to see?")
+st.title(f"{select_data} for the next {forecast_days} day/s in {place}")
