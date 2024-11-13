85a9f5750384f0a2c59c8a92952168ee8a7218e8
Ori <disori777@gmail.com>
2024-11-13 10:00:13 +0800

    Change title to subheader for better design

--- main.py
+++ main.py
@@ -6,4 +6,4 @@ place = st.text_input("Place:", placeholder="Input a place to view data")
 forecast_days = st.slider("Forecast Days", 1, 5, 1, help="Select the number of forecasted days")
 data_choice = ["Temperature", "Weather"]
 select_data = st.selectbox("Select data to view", data_choice, placeholder="What data you want to see?")
-st.title(f"{select_data} for the next {forecast_days} day/s in {place}")
+st.subheader(f"{select_data} for the next {forecast_days} day/s in {place}")
