999a787d076fbda561de254a3d625d03ba72c145
Ori <disori777@gmail.com>
2024-11-13 09:59:55 +0800

    Add help function in forecast day slider

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import streamlit as st
 
 st.title("Weather Forecast for the Next Days")
 place = st.text_input("Place:", placeholder="Input a place to view data")
-forecast_days = st.slider("Forecast Days", 1, 5, 1)
+forecast_days = st.slider("Forecast Days", 1, 5, 1, help="Select the number of forecasted days")
 data_choice = ["Temperature", "Weather"]
 select_data = st.selectbox("Select data to view", data_choice, placeholder="What data you want to see?")
 st.title(f"{select_data} for the next {forecast_days} day/s in {place}")
