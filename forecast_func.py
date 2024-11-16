f4c576e58a776eee9c72d49b2fe66a19bb125f54
Ori <disori777@gmail.com>
2024-11-16 09:40:39 +0800

    Remove temporary function

--- forecast_func.py
+++ forecast_func.py
@@ -1,5 +1,3 @@
-def get_temperature_data(days):
-    temporary_dates = ["2020", "2021", "2022", "2023", "2024"]
-    temporary_temperatures = [10, 12, 25, 23, 21]
-    temporary_temperatures = [days * x for x in temporary_temperatures]
-    return temporary_dates, temporary_temperatures
+
+
+def get_data(place, days, type):
