587b686ff52228d2de1d3bc4e74d111558e1822d
Ori <disori777@gmail.com>
2024-11-16 10:42:20 +0800

    Implement feature that returns temperature data per hour, based on the forecast days given.

--- forecast_func.py
+++ forecast_func.py
@@ -1,3 +1,18 @@
+import requests
 
 
-def get_data(place, days, type):
+API_KEY = '36gepPHxgAQvHJQo005ntzieNfuS9m3w'
+
+def get_temperature_data(place, days):
+    url = f"https://api.tomorrow.io/v4/weather/forecast?location={place}&apikey={API_KEY}"
+    response = requests.get(url)
+    content = response.json()
+    nr_data_values = days * 24
+    hourly_data = content['timelines']['hourly']
+    filtered_hourly_data = hourly_data[:nr_data_values]
+    temperature_data = [temp['values']['temperature'] for temp in filtered_hourly_data]
+    time_data = [hour['time'][11:16] for hour in filtered_hourly_data]
+    return temperature_data, time_data
+
+if __name__ == "__main__":
+    print(get_temperature_data('Dagupan', 1))
