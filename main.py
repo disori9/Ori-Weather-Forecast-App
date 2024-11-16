75b4b673d98c90cb229a80e989c669e7d6f6c6b6
Ori <disori777@gmail.com>
2024-11-16 12:07:28 +0800

    Separate graphs into two separate if blocks based on the data_choice given by the user in select box

--- main.py
+++ main.py
@@ -7,8 +7,14 @@ place = st.text_input("Place:", placeholder="Input a place to view data")
 forecast_days = st.slider("Forecast Days", 1, 5, 1, help="Select the number of forecasted days")
 data_choice = ["Temperature", "Weather"]
 select_data = st.selectbox("Select data to view", data_choice, index=1, placeholder="What data you want to see?")
-st.subheader(f"{select_data} for the next {forecast_days} day/s in {place}")
 
-dates, temperatures = ff.get_temperature_data(forecast_days)
-temp_figure = px.line(x=dates, y=temperatures, labels={"x": "Date", "y": "Temperature (c)"})
-st.plotly_chart(temp_figure)
+st.subheader(f"{select_data} for the next {forecast_days} day/s in {place}")
+if select_data == "Temperature":
+    temperatures, times = ff.get_temperature_data(place, forecast_days)
+    temp_figure = px.line(x=times, y=temperatures, labels={"x": "Date", "y": "Temperature (c)"})
+    st.plotly_chart(temp_figure)
+
+if select_data == "Weather":
+    weather, times = ff.get_weather_data(place, forecast_days)
+    temp_figure = px.line(x=times, y=weather, labels={"x": "Date", "y": "Temperature (c)"})
+    st.plotly_chart(temp_figure)
